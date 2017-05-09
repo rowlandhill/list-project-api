dc05bf476f7bca7fcf93d4224d4d7d0de93ce315
rowlandhill <hill.rowland@gmail.com>
2017-05-09 18:14:10 -0400

    Cleans up code, no changes made, just fixing linter errors that did not affect back-end

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -45,20 +45,20 @@ class RecipesController < ProtectedController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      # require 'pry'
-      # binding.pry
-        @recipe = current_user.recipes.find(params[:id])
-      # @recipe = Recipe.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipe
+    # require 'pry'
+    # binding.pry
+    @recipe = current_user.recipes.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
+  end
 
-    def validate_user
-      set_current_user
-    end
+  def validate_user
+    set_current_user
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def recipe_params
-      params.require(:recipe).permit(:name, :description, :servings).reject { |_, v| v.blank? }
-    end
+  # Only allow a trusted parameter "white list" through.
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :servings).reject { |_, v| v.blank? }
+  end
 end
